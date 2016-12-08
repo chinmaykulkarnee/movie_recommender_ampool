2153e106bafa5a968d2b57a6251e5e7983b0ad97
Chinmay Kulkarni <chinmaykulkarnee@gmail.com>
2016-12-08 18:17:18 +0530

    Generate 25 highest rated movie recommendation

--- src/main/java/io/ampool/SparkQueryRunner.java
+++ src/main/java/io/ampool/SparkQueryRunner.java
@@ -9,13 +9,20 @@ import org.apache.spark.api.java.function.Function;
 import org.apache.spark.mllib.recommendation.ALS;
 import org.apache.spark.mllib.recommendation.MatrixFactorizationModel;
 import org.apache.spark.mllib.recommendation.Rating;
+import org.apache.spark.rdd.RDD;
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
+import scala.Function1;
 import scala.Tuple2;
+import scala.math.Ordering;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by adongre on 10/17/16.
@@ -38,21 +45,75 @@ public class SparkQueryRunner {
         options.put("ampool.locator.port", String.valueOf(locatorPort));
 
         DataFrame ratingsInfoDF = sqlContext.read().format("io.ampool").options(options).load(RATINGS_INFO_TABLE);
-//        DataFrame moviesInfoDF = sqlContext.read().format("io.ampool").options(options).load(MOVIES_INFO_TABLE);
-
 //        ratingsInfoDF.show();
-//        moviesInfoDF.show();
 
         DataFrame[] dataFrames = ratingsInfoDF.randomSplit(new double[]{6, 2, 2}, 0);
         JavaRDD<Rating> trainingRDD = convertDFtoRatingsRDD(dataFrames[0]);
         JavaRDD<Rating> validationRDD = convertDFtoRatingsRDD(dataFrames[1]);
         JavaRDD<Rating> testRDD = convertDFtoRatingsRDD(dataFrames[2]);
 
-        MatrixFactorizationModel model = buildRecommendationModel(trainingRDD, validationRDD);
+//        MatrixFactorizationModel model = buildRecommendationModel(trainingRDD, validationRDD);
 
-        double testError = evaluateModel(testRDD, model);
-        System.out.println("The error for test data is " + testError);
+//        double testError = evaluateModel(testRDD, model);
+//        System.out.println("The error for test data is " + testError);
 
+        DataFrame moviesInfoDF = sqlContext.read().format("io.ampool").options(options).load(MOVIES_INFO_TABLE);
+        JavaRDD<Tuple2<Integer, String>> moviesTitlesRDD = moviesInfoDF.javaRDD().map(
+                (Function<Row, Tuple2<Integer, String>>) row -> new Tuple2<Integer, String>(row.getInt(0), row.getString(1)));
+
+        JavaPairRDD<Integer, Iterable<Tuple2<Integer, Double>>> moviesIdWithRatingsRDD = ratingsInfoDF.javaRDD().map(
+                row -> new Tuple2<>(row.getInt(1), row.getDouble(2))
+        ).groupBy(tuple -> tuple._1);
+
+        JavaRDD<Tuple2<Integer, Tuple2<Integer, Double>>> movieIdWithAvgRatingsRDD = moviesIdWithRatingsRDD.map(idRatingsTuple -> {
+            int noOfRatings = 0;
+            double sumOfRatings = 0, avgRating = 0;
+            for (Tuple2<Integer, Double> tuple : idRatingsTuple._2) {
+                sumOfRatings += tuple._2;
+                noOfRatings++;
+            }
+            avgRating = sumOfRatings / noOfRatings;
+            return new Tuple2<>(idRatingsTuple._1, new Tuple2<>(noOfRatings, avgRating));
+        });
+
+        JavaRDD<Tuple2<Integer, Integer>> movieRatingCountsRDD = movieIdWithAvgRatingsRDD.map(tuple -> new Tuple2<>(tuple._1, tuple._2._1));
+
+        int[][] newUserRatings = {
+                {0, 260, 4}, // Star Wars (1977)
+                {0, 1, 3}, // Toy Story (1995)
+                {0, 16, 3}, // Casino (1995)
+                {0, 25, 4}, // Leaving Las Vegas (1995)
+                {0, 32, 4}, // Twelve Monkeys (a.k.a. 12 Monkeys) (1995)
+                {0, 335, 1}, // Flintstones, The (1994)
+                {0, 379, 1}, // Timecop (1994)
+                {0, 296, 3}, // Pulp Fiction (1994)
+                {0, 858, 5}, // Godfather, The (1972)
+                {0, 50, 4} // Usual Suspects, The (1995)
+        };
+        JavaRDD<Rating> newUserRatingsRDD = jsc.parallelize(Arrays.asList(newUserRatings)).map(arr -> new Rating(arr[0], arr[1], arr[2]));
+        JavaRDD<Rating> allRatingsWithNewUserRatings = convertDFtoRatingsRDD(ratingsInfoDF).union(newUserRatingsRDD);
+        MatrixFactorizationModel newModel = ALS.train(JavaRDD.toRDD(allRatingsWithNewUserRatings), 4, 10, 0.01);
+
+        List newUserRatedMovieIds = Arrays.asList(newUserRatings).parallelStream().map(arr -> arr[1]).collect(Collectors.toList());
+        JavaRDD<Row> newUserUnratedMovies = moviesInfoDF.toJavaRDD().filter(
+                row -> !newUserRatedMovieIds.contains(row.getInt(0))
+        );
+        JavaRDD<Tuple2<Object, Object>> x1 = newUserUnratedMovies.map((Function<Row, Tuple2<Object, Object>>) row -> new Tuple2<Object, Object>(0, row.getInt(0)));
+
+        RDD<Rating> newUserRecommendationsRDD = newModel.predict(JavaRDD.toRDD(x1));
+        JavaRDD<Tuple2<Integer, Double>> newUserRecommendationsRatingsRDD = newUserRecommendationsRDD.toJavaRDD().map(
+                (Function<Rating, Tuple2<Integer, Double>>) r -> new Tuple2<>(r.product(), r.rating())
+        );
+        JavaPairRDD<Integer, Tuple2<Tuple2<Double, String>, Integer>> newUserRecommendationsRatingTitleAndCountRDD = JavaPairRDD.fromJavaRDD(newUserRecommendationsRatingsRDD).
+                join(JavaPairRDD.fromJavaRDD(moviesTitlesRDD)).
+                join(JavaPairRDD.fromJavaRDD(movieRatingCountsRDD));
+
+        JavaRDD<Tuple2<Double, String>> filteredMovieTitleAndRatingsRDD = newUserRecommendationsRatingTitleAndCountRDD.
+                filter(tuple -> tuple._2._2 > 25).
+                map(tuple -> new Tuple2<>(tuple._2._1._1, tuple._2._1._2));
+
+        System.out.println(filteredMovieTitleAndRatingsRDD.
+                sortBy(doubleStringTuple2 -> doubleStringTuple2._1, false, 1).take(5));
     }
 
     private MatrixFactorizationModel buildRecommendationModel(JavaRDD<Rating> trainingRDD, JavaRDD<Rating> validationRDD) {
